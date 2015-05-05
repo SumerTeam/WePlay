0fed57bc75af69f3079bca1a8adc065bd0e12992
fly <439892188@qq.com>
2015-05-05 14:12:18 +0800

    添加路由

---
+++ route/interface.js
@@ -0,0 +1,24 @@
+/**
+ * Created by liusifei on 15/5/5.
+ */
+
+exports.action = function (req, res) {
+    try {
+        var command = req.param('command');
+        if (command && this[command]) {
+            this[command](req, function (response) {
+                res.send(response);
+            })
+        } else {
+            throw ({code: 9999, message: "command is not exist"});
+        }
+    } catch (e) {
+        console.log("err is:", e);
+    }
+};
+
+//注册
+var register = require('../lib/register').register;
+
+//登录
+var login = require('../lib/login').login;
